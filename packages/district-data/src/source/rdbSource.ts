97d9a0335410d47eeab32e2f9275b943c8009fa1
hsp-sz <HSimple1999@163.com>
2024-06-03 11:25:09 +0800

    feat: 修改district-data镜像源

--- packages/district-data/src/source/rdbSource.ts
+++ packages/district-data/src/source/rdbSource.ts
@@ -20,7 +20,7 @@ const DataConfig = {
     text: '锐多宝的地理空间',
     href: 'https://github.com/ruiduobao/shengshixian.com',
   },
-  url: 'https://jsd.onmicrosoft.cn/npm/xingzhengqu',
+  url: 'https://unpkg.alibaba-inc.com/xingzhengqu',
   // url: 'https://unpkg.com/xingzhengqu',
 };
 
